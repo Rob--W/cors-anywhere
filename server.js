8a27a896d41db8bd63dbe17cbb7bb4a4daf399e1
Rob W <gwnRob@gmail.com>
2013-01-03 21:14:12 +0100

    Detect port from environment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var host = '127.0.0.1';
-var port = 8080;
+// Heroku defines the environment variable PORT, and requires the binding address to be 0.0.0.0
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 8080;
 
 var cors_proxy = require("./lib/cors-anywhere");
 cors_proxy.createServer({
