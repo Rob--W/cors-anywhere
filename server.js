4ef5a26cfb1cb969a360bbd06d74d4330a47a5a1
lindsaymacvean <lindsaymacvean@gmail.com>
2018-09-13 23:39:17 +0100

    Trying to find out why Whitelist var is not working

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+console.log(originWhitelist);
 function parseEnvList(env) {
   if (!env) {
     return [];
