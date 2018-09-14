f5df8132973cebbdcfffa6d6fe20f626674a9b73
lindsaymacvean <lindsaymacvean@gmail.com>
2018-09-14 03:18:01 +0100

    added final fix to get correct response header

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ cors_proxy.createServer({
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
-    'cookie',
-    'cookie2',
+    // 'cookie',
+    // 'cookie2',
     // Strip Heroku-specific headers
     'x-heroku-queue-wait-time',
     'x-heroku-queue-depth',
