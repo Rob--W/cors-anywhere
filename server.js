5dbd10f61c3aac4468becce81521ca07bc4cc9a2
François Darveau <franklebest07@gmail.com>
2018-12-28 12:22:12 -0500

    add handling of Referer header in case Origin header was not set by the browser

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: ['origin', 'x-requested-with', 'referer'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
