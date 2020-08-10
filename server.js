a323e7526f79954ed823b9524f906fe05c558d5a
Adriane Justine Tan <eidoriantan@gmail.com>
2020-08-10 13:15:47 +0800

    perf: count unblocked requests for total instead

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-  logRequests: true,
+  countRequest: true,
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
